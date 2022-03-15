364221cce5c11981f811c9fcba35c3233f8c7be2
HARRiE_Dev <62064385+HARRiE13-Dev@users.noreply.github.com>
2022-03-16 05:46:49 +0700

    Day1 80percens to sleep

--- web/src/App.js
+++ web/src/App.js
@@ -4,37 +4,95 @@ import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const baseURL = "http://localhost:7777/api/trip"
+const baseURL = `http://localhost:7777/api/trip`;
 
 function App() {
+  const [trips, setTrips] = useState([]);
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    // request to api by axios
+    
+    axios.get(`baseURL/keyword/${keyword}`).then((response) => {
+      setTrips(response.data);
+    });
+  }, []);
+
   return (
     <>
-      <div class="style-font">
-        <header class="text-center">
+      <div className="style-font">
+        <header className="text-center">
           <h1 className="font_header">เที่ยวไหนดี</h1>
           <input
-            class="style-font text-center"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            className="style-font text-center"
             type="text"
             name=""
             placeholder="หาที่เที่ยวแล้วไปกัน..."
           />
+          {keyword}
         </header>
-
-        <div class="grid">
-          <div class="item">
-            <div class="content">
-              <p>11111</p>
+        {/* Loop for request api from search */}
+        {trips.map((element) => {
+          return (
+            <div className="grid mb-3" key={element.eid}>
+              <div className="item">
+                <div className="content">
+                  <img
+                    className="crop shadow-lg"
+                    src={element.photos[0]}
+                    alt=""
+                  ></img>
+                </div>
+              </div>
+              <div className="item">
+                <div className="content ">
+                  <h3 className="font_topic text-left truncate">
+                    {element.title}
+                  </h3>
+                  <div className="font_detail text-left">
+                    <p className="truncate-3 ">{element.description}</p>
+                    พร้อมแล้วก็ไปเลย ช้าหมดอดมันส์กับเพื่อน ....{" "}
+                    <a className="read-more" href="#">
+                      อ่านต่อ
+                    </a>
+                  </div>
+                  <div className="font_detail text-left">
+                    <p className="text-left font_tags">
+                      หมวด - <a href="#">{element.tags[0]}</a>{" "}
+                      <a href="#">{element.tags[1]}</a> และ{" "}
+                      <a href="#">{element.tags[2]}</a>
+                    </p>
+                  </div>
+                  <div className="grid-img">
+                    <div className="item">
+                      <img
+                        className="crop-square shadow-lg"
+                        src={element.photos[1]}
+                        alt=""
+                      ></img>
+                    </div>
+                    <div className="item">
+                      <img
+                        className="crop-square shadow-lg"
+                        src={element.photos[2]}
+                        alt=""
+                      ></img>
+                    </div>
+                    <div className="item">
+                      <img
+                        className="crop-square shadow-lg"
+                        src={element.photos[3]}
+                        alt=""
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="item">
-            <div class="content">
-              <p>22222</p>
-            </div>
-          </div>
-        </div>
-      
-      
-
+          );
+        })}
       </div>
     </>
   );
