22f5eea0fec2b43eed1a5531fb887631dac574bb
HARRiE_Dev <62064385+HARRiE13-Dev@users.noreply.github.com>
2022-03-16 14:36:39 +0700

    changed Profile

--- web/src/App.js
+++ web/src/App.js
@@ -11,21 +11,26 @@ function App() {
   const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
-    // request to api by axios
-    
-    axios.get(`${baseURL}`).then((response) => {
-      setTrips(response.data);
-    });
+    //
   }, []);
 
-  function getData(val) {
-    setKeyword(val.target.value)
-    // axios.get(`http://localhost:7777/api/trip/keyword/${keyword}`).then((response) => {
-    //   setTrips(response.data);
-    // });
-  }
-
-
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      if (keyword == "") {
+        axios.get(`${baseURL}`).then((response) => {
+          setTrips(response.data);
+        });
+      } else if (keyword != "") {
+        axios.get(`${baseURL}/keyword/${keyword}`).then((response) => {
+          if (response.data.length > 0) {
+            setTrips(response.data);
+          } else if (response.data.length == 0) {
+            alert("No trip found");
+          }
+        });
+      }
+    }
+  };
 
   return (
     <>
@@ -33,74 +38,79 @@ function App() {
         <header className="text-center">
           <h1 className="font_header">เที่ยวไหนดี</h1>
           <input
+            onKeyPress={handleKeyPress}
             value={keyword}
-            // onChange={(e) => setKeyword(e.target.value)}
-            onChange={(val) => getData(val)}
+            onChange={(event) => {
+              setKeyword(event.target.value);
+            }}
             className="style-font text-center"
             type="text"
             name=""
             placeholder="หาที่เที่ยวแล้วไปกัน..."
           />
-          {keyword}
         </header>
         {/* Loop for request api from search */}
         {trips.map((element) => {
           return (
-            <div className="grid mb-3" key={element.eid}>
-              <div className="item">
-                <div className="content">
-                  <img
-                    className="crop shadow-lg"
-                    src={element.photos[0]}
-                    alt=""
-                  ></img>
-                </div>
-              </div>
-              <div className="item">
-                <div className="content ">
-                  <h3 className="font_topic text-left truncate">
-                    {element.title}
-                  </h3>
-                  <div className="font_detail text-left">
-                    <p className="truncate-3 ">{element.description}</p>
-                    พร้อมแล้วก็ไปเลย ช้าหมดอดมันส์กับเพื่อน ....{" "}
-                    <a className="read-more" href="#">
-                      อ่านต่อ
-                    </a>
-                  </div>
-                  <div className="font_detail text-left">
-                    <p className="text-left font_tags">
-                      หมวด - <a href="#">{element.tags[0]}</a>{" "}
-                      <a href="#">{element.tags[1]}</a> และ{" "}
-                      <a href="#">{element.tags[2]}</a>
-                    </p>
-                  </div>
-                  <div className="grid-img">
-                    <div className="item">
-                      <img
-                        className="crop-square shadow-lg"
-                        src={element.photos[1]}
-                        alt=""
-                      ></img>
-                    </div>
-                    <div className="item">
-                      <img
-                        className="crop-square shadow-lg"
-                        src={element.photos[2]}
-                        alt=""
-                      ></img>
-                    </div>
-                    <div className="item">
-                      <img
-                        className="crop-square shadow-lg"
-                        src={element.photos[3]}
-                        alt=""
-                      ></img>
-                    </div>
+            <>
+              <div className="grid mb-3" key={element.eid}>
+                <div className="item">
+                  <div className="content">
+                    <img
+                      className="crop shadow-lg"
+                      src={element.photos[0]}
+                      alt=""
+                    ></img>
                   </div>
                 </div>
+                <div className="item">
+                  <div className="content ">
+                    <h3 className="font_topic text-left truncate">
+                      <a className="read-more topic-link" href={element.url}>
+                        {element.title}
+                      </a>
+                    </h3>
+                    <div className="font_detail text-left">
+                      <p className="truncate-3 ">{element.description}</p>
+                      พร้อมแล้วก็ไปเลย ช้าหมดอดมันส์กับเพื่อน ....{" "}
+                      <a className="read-more" href={element.url}>
+                        อ่านต่อ
+                      </a>
+                    </div>
+                    <div className="font_detail text-left">
+                      <p className="text-left font_tags">
+                        หมวด - <a href="#">{element.tags[0]}</a>{" "}
+                        <a href="#">{element.tags[1]}</a> และ{" "}
+                        <a href="#">{element.tags[2]}</a>
+                      </p>
+                    </div>
+                    <div className="grid-img">
+                      <div className="item">
+                        <img
+                          className="crop-square shadow-lg"
+                          src={element.photos[1]}
+                          alt=""
+                        ></img>
+                      </div>
+                      <div className="item">
+                        <img
+                          className="crop-square shadow-lg"
+                          src={element.photos[2]}
+                          alt=""
+                        ></img>
+                      </div>
+                      <div className="item">
+                        <img
+                          className="crop-square shadow-lg"
+                          src={element.photos[3]}
+                          alt=""
+                        ></img>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
+            </>
           );
         })}
       </div>
